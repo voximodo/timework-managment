1427b90b829ce444bdbc348ff6957a07b5f6c69e
voximPC <voximodo@gmail.com>
2018-01-08 22:12:10 +0100

    - added registering card with get request

--- karty_projekt/urls.py
+++ karty_projekt/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from timework.views import reg_entrence, get_users
+from timework.views import reg_entrence, get_users, reg_card
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^register_entrence/$', reg_entrence),
+    url(r'^register_card/$', reg_card),
     url(r'^get_users/$', get_users),
 ]
--- timework/views.py
+++ timework/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Worker
+from .models import Worker, Card
 
 def reg_entrence(request):
     return HttpResponse("Hello world")
+
+def reg_card(request):
+    if 'w' in request.GET:
+        id = int(request.GET['w'])
+        worker = Worker.objects.get(id=id)
+        if 'c' in request.GET:
+            uid = request.GET['c']
+            card = Card(worker=worker, uid=uid)
+            card.save()
+            return HttpResponse("Pomyślnie zarejestrowano kartę.")
 
 def get_users(request):
     workers = Worker.objects.all()
     response = " "
 
     for worker in workers:
-        response = response + "<li>" + worker.first_name + " " + worker.second_name + "</li>"
+        response = response + "<li>" + str(worker.id) + "." + worker.first_name + " " + worker.second_name + "</li>"
 
     return HttpResponse(response)
